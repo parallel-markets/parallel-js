8ee1b7c6eabd6941fdd3d9abeba9073c5d36b98a
Brian Muller <bmuller@parallelmarkets.com>
2022-10-30 20:19:48 -0400

    update examples to show auth data

--- examples/react-webpack/src/index.js
+++ examples/react-webpack/src/index.js
@@ -1,39 +1,60 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { loadParallel } from '@parallelmarkets/vanilla'
-import {
-  ParallelProvider,
-  useParallel,
-  PassportButton,
-} from '@parallelmarkets/react'
+import { ParallelProvider, useParallel, PassportButton } from '@parallelmarkets/react'
 
-const AccreditationArea = () => {
+// replace the client_id with your client id
+const scopes = ['profile', 'accreditation_status']
+const parallel = loadParallel({ client_id: 'VpbHft3b8bbKTv2LyaUVS', environment: 'demo', flow_type: 'overlay', scopes })
+
+const ProfileBox = () => {
+  const { loginStatus, getProfile } = useParallel()
+  const [profileResponse, setProfileResponse] = useState(null)
+
+  useEffect(() => {
+    // if the user is now connected, then fetch profile info
+    if (loginStatus?.status === 'connected') getProfile().then(setProfileResponse)
+  }, [loginStatus])
+
+  if (!profileResponse) return null
+
+  if (profileResponse.type === 'individual') {
+    const { profile: { first_name, last_name, email } } = profileResponse
+    return <h3>{first_name} {last_name} &lt;{email}&gt;</h3>
+  } else {
+    const { profile: { name } } = profileResponse
+    return <h3>{name} (business)</h3>
+  }
+}
+
+const ConnectionArea = () => {
   const { parallel, loginStatus } = useParallel()
 
   // we may render before the loginStatus is available
   if (!loginStatus) return null
 
-  console.log(loginStatus)
+  // if we got the status, but it's not connected, show the connection button
+  if (loginStatus.status !== 'connected') return <PassportButton />
 
   return (
     <>
-      <h1>Status: {loginStatus.status}</h1>
-      {loginStatus.status !== 'connected' ? (
-        <PassportButton />
-      ) : (
-        <button onClick={parallel.logout}>Log Out</button>
-      )}
+      <h2>Connection successful!</h2>
+      <ProfileBox />
+      <div style={{ padding: '15px', margin: '15px', backgroundColor: '#eee', overflow: 'wrap', overflowWrap: 'break-word' }}>
+        <h3>Authentication Response Parameters:</h3>
+        <ul>
+          {Object.entries(loginStatus.authResponse).map(([key, value], i) => <li key={i}><b>{key}:</b> {value}</li>)}
+        </ul>
+      </div>
+      <button onClick={parallel.logout}>Log Out</button>
     </>
   )
 }
 
-// replace the client_id with your client id
-const parallel = loadParallel({ client_id: 'VpbHft3b8bbKTv2LyaUVS', environment: 'demo', flow_type: 'overlay' })
-
 const App = () => (
   <ParallelProvider parallel={parallel}>
-    <AccreditationArea />
+    <ConnectionArea />
   </ParallelProvider>
 )
 
--- examples/webpack/src/index.js
+++ examples/webpack/src/index.js
@@ -2,12 +2,14 @@ import { loadParallel } from '@parallelmarkets/vanilla'
 
 (async () => {
   // Replace client_id with your client id
-  const Parallel = await loadParallel({ client_id: 'VpbHft3b8bbKTv2LyaUVS', environment: 'demo', flow_type: 'overlay' })
+  const scopes = ['profile', 'accreditation_status']  
+  const Parallel = await loadParallel({ client_id: 'VpbHft3b8bbKTv2LyaUVS', environment: 'demo', flow_type: 'overlay', scopes })
   
-  Parallel.subscribeWithButton(() => {
-    document.getElementById("message").innerHTML = 'Thanks for sharing your status.'
+  Parallel.subscribeWithButton(({ authResponse }) => {
+    document.getElementById('message').innerHTML = 'You are now connected!'
+    document.getElementById('authResponse').innerHTML = JSON.stringify(authResponse)
   }, (result) => {
     console.error(result)
-    document.getElementById("message").innerHTML = 'Looks like there was an issue.'
+    document.getElementById('message').innerHTML = 'Looks like there was an issue.'
   })
 })();
